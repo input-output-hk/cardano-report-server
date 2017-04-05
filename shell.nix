a49f4774c556f68b80259587fa88757046b97a1b
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-04-05 15:51:11 +0300

    Switch to lts-8.5, bump shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> { };
 haskell.lib.buildStackProject {
    ghc = haskell.packages.ghc802.ghc;
    name = "cardano-report-server";
-   buildInputs = [ zlib git openssh ];
+   buildInputs = [ zlib git openssh gmp ];
    LANG = "en_US.UTF-8";
 }
 

d48596ea0dbef9f2c13f22a68a4f0f8dcab96097
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-01-30 22:16:02 +0300

    [CSL-2232] Cosmetics

--- src/Pos/ForwardClient/Client.hs
+++ src/Pos/ForwardClient/Client.hs
@@ -1,24 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Pos.ForwardClient.Client where
+module Pos.ForwardClient.Client
+    ( getAgentID
+    , uploadLogs
+    , createTicket
+    , defCr
+    ) where
 
-import Prelude
+import           Universum
 
-import Network.Wreq (Options, defaults, auth, basicAuth, 
-                     header, getWith, postWith, responseBody,
-                     partFile)
-import Control.Lens
--- import Control.Monad
-import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Lazy  as BSL
+import           Control.Lens ((?~))
+import           Data.Aeson.Lens (key, _Integer, _String)
+import qualified Data.ByteString.Lazy as BSL
+import           Network.Wreq (Options, auth, basicAuth, defaults, getWith, header, partFile,
+                               postWith, responseBody)
 
-import Data.Aeson.Lens
-
-import qualified Data.Text as T
-import Data.Text.Encoding (encodeUtf8)
-
-import Pos.ForwardClient.Types
-import Pos.ReportServer.Util (prettifyJson)
+import           Pos.ForwardClient.Types (Agent (..), CrTicket (..), CustomReport (..), Token)
+import           Pos.ReportServer.Util (prettifyJson)
 
 -- defAgent :: Agent
 -- defAgent = Agent "stavrikios@gmail.com" "V8j8Kv3w5ZJY5huaT1BlwChybe5cZsh3ykVg8uG6" 360221985593
@@ -30,37 +28,43 @@ defCr :: CustomReport
 defCr = CustomReport "user@email.com" "sample problem" "sample description"
 
 agentOpts :: Agent -> Options
-agentOpts (Agent email token _) = defaults
-                                & auth ?~ basicAuth (encodeUtf8 (T.append email "/token")) (encodeUtf8 token)
+agentOpts (Agent email token _) =
+    defaults & auth ?~ basicAuth (encodeUtf8 (email <> "/token")) (encodeUtf8 token)
 
 agentOptsJson :: Agent -> Options
-agentOptsJson (Agent email token _) = defaults
-                                    & header "content-type" .~ ["application/json"]
-                                    & auth ?~ basicAuth (encodeUtf8 (T.append email "/token")) (encodeUtf8 token)
+agentOptsJson (Agent email token _) =
+    defaults & header "content-type" .~ ["application/json"]
+             & auth ?~ basicAuth (encodeUtf8 (email <> "/token")) (encodeUtf8 token)
 
 -- | Queries the zendesk api to get the agent's id
 getAgentID :: Agent -> IO Integer
 getAgentID agent = do
     r <- getWith (agentOpts agent) (api ++ "locales/current.json")
-    let Just tok = r ^? responseBody . key "locale" . key "id" . _Integer
-    return tok
-
-joinLogs :: [(FilePath, BSL.ByteString)] -> BSL.ByteString
-joinLogs logs = BSL.concat $ snd <$> logs
+    let tok =
+            fromMaybe (error "getAgentId: couldn't retrieve locale.id field") $
+            r ^? responseBody . key "locale" . key "id" . _Integer
+    pure tok
 
 -- | Merges the log files, uploads them to Zendesk and returns the token from zendesk.
 -- The name of the upload is defaulted to logs.log
-uploadLogs :: Agent -> [(FilePath, BSL.ByteString)] -> IO Token
+uploadLogs :: Agent -> [(FilePath, LByteString)] -> IO Token
 uploadLogs agent logs =  do
     BSL.writeFile "logs.log" $ joinLogs logs
-    r <- postWith (agentOpts agent) (api ++ "uploads.json?filename=logs.log") (partFile "log" "logs.log")
+    r <- postWith (agentOpts agent)
+                  (api ++ "uploads.json?filename=logs.log")
+                  (partFile "log" "logs.log")
     let Just tok = r ^? responseBody . key "upload" . key "token" . _String
-    return tok
+    pure tok
+  where
+    joinLogs :: [(FilePath, LByteString)] -> LByteString
+    joinLogs = mconcat . map snd
 
-createTicket :: Agent -> CustomReport -> [(FilePath, BSL.ByteString)] -> IO BSL.ByteString
+createTicket :: Agent -> CustomReport -> [(FilePath, LByteString)] -> IO LByteString
 createTicket agent cr logs = do
     attachToken <- uploadLogs agent logs
     let ticket = CrTicket agent cr attachToken
-    putStrLn $ C.unpack $ encodeUtf8 $ prettifyJson ticket
-    r <- postWith (agentOptsJson agent) (api ++ "tickets.json") (encodeUtf8 (prettifyJson ticket))
-    return $ r ^. responseBody
+    putStrLn $ prettifyJson ticket
+    r <- postWith (agentOptsJson agent)
+                  (api ++ "tickets.json")
+                  (encodeUtf8 (prettifyJson ticket) :: ByteString)
+    pure $ r ^. responseBody
--- src/Pos/ForwardClient/Types.hs
+++ src/Pos/ForwardClient/Types.hs
@@ -1,48 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Pos.ForwardClient.Types where
+-- | Types for sending data to zendesk.
+module Pos.ForwardClient.Types
+    ( CustomReport(..)
+    , Agent(..)
+    , Logs
+    , Token
+    , CrTicket(..)
+    ) where
 
-import Prelude
+import           Universum
 
-import           Data.ByteString.Lazy as BSL
-import           Data.Text
-import qualified Data.Vector          as V
-import           Data.Aeson           (ToJSON (..), Value (Array), object, (.=))
+import           Data.Aeson (ToJSON (..), Value (Array), object, (.=))
+import qualified Data.Vector as V
 
-data CustomReport = CustomReport {
-    crEmail       :: Text
-  , crSubject     :: Text
-  , crDescription :: Text
-} deriving Show
+data CustomReport = CustomReport
+    { crEmail       :: Text
+    , crSubject     :: Text
+    , crDescription :: Text
+    } deriving (Show)
 
-data Agent = Agent {
-    aEmail    :: Text
-  , apiToken  :: Token
-  , aId       :: Integer
-} deriving Show
+data Agent = Agent
+    { aEmail   :: Text
+    , apiToken :: Token
+    , aId      :: Integer
+    } deriving (Show)
 
-type Logs = [(FilePath, BSL.ByteString)]
+type Logs = [(FilePath, LByteString)]
 
 type Token = Text
 
-data CrTicket = CrTicket {
-    tAgent        :: Agent
-  , tCustomReport :: CustomReport
-  , tAttachment   :: Token
-} deriving Show
+data CrTicket = CrTicket
+    { tAgent        :: Agent
+    , tCustomReport :: CustomReport
+    , tAttachment   :: Token
+    } deriving (Show)
 
 instance ToJSON CrTicket where
   toJSON CrTicket {..} =
-    object [ "ticket" .=
-      object [ "type"          .= ("custom report" :: Text)
-             , "subject"       .= crSubject tCustomReport
-             , "description"   .= crDescription tCustomReport
-             , "requester_id"  .= aId tAgent
-             , "assignee_id"   .= aId tAgent
-             , "custom_fields" .= Array (V.fromList [ object [ "id" .= (1 :: Integer), "value" .= crEmail tCustomReport]])
-             , "comment"       .=
-                  object [ "type"    .= ("Attached logs" :: Text)
-                         , "uploads" .= V.singleton tAttachment
-                         ]
-             ]
-    ]
+      object [ "ticket" .=
+          object [ "type"          .= ("custom report" :: Text)
+                 , "subject"       .= crSubject tCustomReport
+                 , "description"   .= crDescription tCustomReport
+                 , "requester_id"  .= aId tAgent
+                 , "assignee_id"   .= aId tAgent
+                 , "custom_fields" .= Array (V.fromList $ one $ object
+                                             [ "id" .= (1 :: Integer)
+                                             , "value" .= crEmail tCustomReport])
+                 , "comment"       .=
+                      object [ "type"    .= ("Attached logs" :: Text)
+                             , "uploads" .= V.singleton tAttachment
+                             ]
+                 ]
+      ]

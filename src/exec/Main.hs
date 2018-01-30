6f4e28613f701052fd651b6d90790b4ae0bc11d0
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-01-30 22:53:39 +0300

    [CSL-2232] Send zd response back to daedalus

--- src/exec/Main.hs
+++ src/exec/Main.hs
@@ -4,13 +4,13 @@
 
 module Main (main) where
 
-import qualified Network.Wai.Handler.Warp             as Warp
+import qualified Network.Wai.Handler.Warp as Warp
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import           Universum
 
-import           Options                              (Opts (..), getOptions)
-import           Pos.ReportServer.FileOps             (initHolder)
-import           Pos.ReportServer.Server              (limitBodySize, reportServerApp)
+import           Options (Opts (..), getOptions)
+import           Pos.ReportServer.FileOps (initHolder)
+import           Pos.ReportServer.Server (limitBodySize, reportServerApp)
 
 main :: IO ()
 main = do
@@ -20,5 +20,5 @@ main = do
     putText "Successfully created holder"
 
     putText "Launching server..."
-    let application = reportServerApp holder
+    let application = reportServerApp holder zendeskAgent
     Warp.run port $ logStdoutDev $ limitBodySize sizeLimit $ application
